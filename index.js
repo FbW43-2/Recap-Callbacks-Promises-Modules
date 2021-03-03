f4740fc4648b2ed78204903009654e2a46997edf
nrcool <nrcoolove@yahoo.com>
2021-03-03 13:40:57 +0100

    async await

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ let originalObj= JSON.parse(jsondata)
 
 // fetching real data from external API
 
-fetch("https://reqres.in/api/users?page=2")//promise (asynchronous)
+/* fetch("https://reqres.in/api/users?page=2")//promise (asynchronous)
+console.log(response)
 .then(response=>response.json())// jsondata response(parse data)
 .then(result=>{
     console.log(result)
@@ -113,4 +114,34 @@ fetch("https://reqres.in/api/users?page=2")//promise (asynchronous)
     })
     
     
-})//final result
+}).catch(err=>console.log(err)) *///final result
+
+
+//async await (syntactical sugar for handling promises)
+
+/* try{} catch(){} */
+//async function always returns you a promise
+async function FetchData(){
+    try{ 
+        let response = await fetch("https://reqres.in/api/users?page=2")
+        let result = await response.json()
+    
+        let ul=document.querySelector("ul")
+        result.data.map(user=>{
+            let firstname=document.createElement("h1")
+            firstname.innerHTML=user.first_name
+    
+            let lastname=document.createElement("div")
+            lastname.innerHTML=user.last_name
+            let email=document.createElement("div")
+            email.innerHTML=user.email
+            let img=document.createElement("img")
+            img.src=user.avatar
+            let Card=document.createElement("div")
+            Card.append(firstname,lastname,email,img)
+            ul.append(Card)
+        })
+    } catch(err){console.log(err)}
+}
+
+FetchData()
