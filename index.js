742b43d56eb3d8a6884010158eb1affcbf0cdab8
nrcool <nrcoolove@yahoo.com>
2021-03-03 12:09:09 +0100

    fetched real data from api and displayed in DOM

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ console.log("end")  */
 
 //converting callback hell into promises
 
-console.log("start")
+/* console.log("start")
 
 function getName(rollnumber){
     return new Promise((resolve,reject)=>{
@@ -84,4 +84,33 @@ getName(105)
 .catch(err=>console.log(err))
 
 
-console.log("end") 
+console.log("end")  */
+let obj={name:"Ali",age:30}
+let jsondata= JSON.stringify(obj)
+let originalObj= JSON.parse(jsondata)
+
+// fetching real data from external API
+
+fetch("https://reqres.in/api/users?page=2")//promise (asynchronous)
+.then(response=>response.json())// jsondata response(parse data)
+.then(result=>{
+    console.log(result)
+    let ul=document.querySelector("ul")
+
+    result.data.map(user=>{
+        let firstname=document.createElement("h1")
+        firstname.innerHTML=user.first_name
+
+        let lastname=document.createElement("div")
+        lastname.innerHTML=user.last_name
+        let email=document.createElement("div")
+        email.innerHTML=user.email
+        let img=document.createElement("img")
+        img.src=user.avatar
+        let Card=document.createElement("div")
+        Card.append(firstname,lastname,email,img)
+        ul.append(Card)
+    })
+    
+    
+})//final result
