d52ac5df9397fe4d4f0ab3732f0c76842d5c106d
nrcool <nrcoolove@yahoo.com>
2021-03-03 10:18:55 +0100

    callbacks

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 //synchronous and asynchronous code
 //blocking and non-blocking
 
-
-console.log("start")
+/* console.log("start")
 
 function getName(rollnumber){
        setTimeout(()=>{
            //send rollnumber to server and against that rollnumber ,get student name from server
-           let name = "Mike"
+           let name = "first"
            console.log(name)
-       },2000)
-    
+       },2000) 
 }
+
 
 getName(102)
 
+console.log("end") */
 
-console.log("end")
 
+
+
+//callBack
+
+console.log("start")
+
+function getName(rollnumber, callback){
+       setTimeout(()=>{
+           //send rollnumber to server and against that rollnumber ,get student name from server
+           let name = "John"
+           callback(name)
+       },2000) 
+}
+function getHobbies(name,callback){
+    setTimeout(()=>{
+        //sending request to server to get student's hobbies
+        let hobbies = ["reading","coding"]
+        callback(hobbies)
+    },2000)
+}
+
+
+//callback hell
+getName(102,(name)=>{
+    console.log(name);
+    getHobbies(name,(hobbies)=>{
+        console.log(hobbies);
+        
+    })
+})
+
+console.log("end") 
